e964478a89d0030d8e03acf1183335b7a7463a72
Yuliya Masechko <yulia.masechko@gmail.com>
2024-04-28 02:03:27 +0300

    Add footer  and  styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import "./App.css";
 import { Audio } from "react-loader-spinner";
+import Weather from "./Weather";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <h1>Weather</h1>
-
-        <Audio
-          height="80"
-          width="80"
-          radius="9"
-          color="yellow"
-          ariaLabel="loading"
-          wrapperStyle
-          wrapperClass
-        />
+        <div className="weather-app">
+          <h1> The Weather </h1>
+          {/* <Weather /> */}
+          <Footer />
+          <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="yellow"
+            ariaLabel="loading"
+            wrapperStyle
+            wrapperClass
+          />
+        </div>
       </div>
     </div>
   );
---
+++ src/Footer.jsx
@@ -0,0 +1,26 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        This project was coded by{" "}
+        <a href="https://github.com/YuliaMas" target="_blank">
+          Yuliya Masechko
+        </a>{" "}
+        from{" "}
+        <a href="https://shecodes.io" target="_blank">
+          SheCodes
+        </a>{" "}
+        The Weather project is on
+        <a href="https://github.com/YuliaMas/react-weather-app" target="_blank">
+          {" "}
+          GitHub
+        </a>{" "}
+        and hosted on
+        <a href="https://julietta-weather-app.netlify.app/" target="_blank">
+          {" "}
+          Netlify
+        </a>
+      </p>
+    </footer>
+  );
+}
