446d4bf23d951e0705ce7b168d50471a7b233374
Yuliya Masechko <yulia.masechko@gmail.com>
2024-04-28 02:18:30 +0300

    Fixed links in Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,20 +3,28 @@ export default function Footer() {
     <footer>
       <p>
         This project was coded by{" "}
-        <a href="https://github.com/YuliaMas" target="_blank">
+        <a href="https://github.com/YuliaMas" target="_blank" rel="noreferrer">
           Yuliya Masechko
         </a>{" "}
         from{" "}
-        <a href="https://shecodes.io" target="_blank">
-          SheCodes
+        <a href="https://shecodes.io" target="_blank" rel="noreferrer">
+          SheCodes.
         </a>{" "}
         The Weather project is on
-        <a href="https://github.com/YuliaMas/react-weather-app" target="_blank">
+        <a
+          href="https://github.com/YuliaMas/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           {" "}
           GitHub
         </a>{" "}
         and hosted on
-        <a href="https://julietta-weather-app.netlify.app/" target="_blank">
+        <a
+          href="https://julietta-weather-app.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           {" "}
           Netlify
         </a>
