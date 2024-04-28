52f0317700abb5bdf7eac650086d2d57278b2811
Yuliya Masechko <yulia.masechko@gmail.com>
2024-04-29 00:15:58 +0300

    Add header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container d-flex flex-column  justify-content-around vw-100 vh-100">
-        <h1 className="pt-4"> The Weather Forecast</h1>
+        <header>
+          <h1 className="pt-4"> The Weather Forecast</h1>
+        </header>
         <div className="weather-app">
           <Weather />
         </div>
