a6736d0e8a52f22c6e927782ed25da90f420bdeb
Yuliya Masechko <yulia.masechko@gmail.com>
2024-04-28 22:55:57 +0300

    Add new div container

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import "./App.css";
 import Weather from "./Weather";
 import Footer from "./Footer";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <div className="weather-app">
-        <h1> The Weather </h1>
-        <Weather />
+      <div className="container d-flex flex-column  justify-content-around vw-100 vh-100">
+        <h1 className="pt-4"> The Weather Forecast</h1>
+        <div className="weather-app">
+          <Weather />
+        </div>
         <Footer />
       </div>
     </div>
   );
 }
-
-export default App;
