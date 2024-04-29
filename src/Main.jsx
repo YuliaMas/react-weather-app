f9fec085505bd4bc54d52c26bd2220d56082affa
Yuliya Masechko <yulia.masechko@gmail.com>
2024-04-29 10:26:02 +0300

    Fix: Add 2 components: WeatherTemperature, FormattedDate

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,35 +1,28 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function Main(props) {
   return (
     <main className="p-0">
       <h1 className="current-city text-capitalize" id="current-city">
-        {props.city.toLowerCase()}
+        {props.info.city.toLowerCase()}
       </h1>
       <div className="current-weather d-flex justify-content-between align-items-end">
         <div>
           <div className="current-temperature-icon">
-            <img src={props.icon} alt={props.description} />
+            <img src={props.info.icon} alt={props.info.description} />
           </div>
-          <h5 className="text-capitalize"> {props.description}</h5>{" "}
+          <h5 className="text-capitalize"> {props.info.description}</h5>{" "}
           <p className="current-details">
-            Humidity: <strong>{props.humidity}%</strong>, Wind:{" "}
-            <strong>{props.wind}km/h</strong>
+            Humidity: <strong>{props.info.humidity}%</strong>, Wind:{" "}
+            <strong>{props.info.wind}km/h</strong>
           </p>
         </div>
         <div className="current-temperature">
           <div>
-            <span
-              className="current-temperature-value"
-              id="current-temperature"
-            >
-              {props.temp}
-            </span>
-            <span className="current-temperature-unit">°C</span>{" "}
-            <span className="current-temperature-unit">|</span>{" "}
-            <span className="current-temperature-unit">°F</span>
-            <FormattedDate date={props.date} />
+            <WeatherTemperature tempCelsius={props.info.temperature} />
+            <FormattedDate date={props.info.date} />
           </div>
         </div>
       </div>
